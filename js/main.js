c88e9b90e69788c7381696227fb9920778f3ab17
Raey Abera <raeyabera@Raeys-MacBook-Pro.local>
2019-03-10 01:18:24 -0500

    second commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
 document.querySelector("button").addEventListener("click", getMovie);
 
 function getMovie() {
-  let movie = document.getElementById("movie").value;
+  let movie = document.getElementById("input").value;
   let apiURL = "http://www.omdbapi.com/?apikey=970fe401&t=" + encodeURI(movie);
 
   fetch(apiURL)
     .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
     .then(response => {
       document.querySelector("img").src = response.Poster;
-      document.querySelector("h2").innerHTML = response.Title;
-      document.querySelector("p").innerHTML = response.Plot;
+      let actors = response.Actors.split(" ");
+      console.log(actors);
+      let actor = actors[0];
+      actorGender(actor);
     })
     .catch(err => {
       console.log(`error ${err}`);
       alert("sorry, no match");
+    });
+}
+function actorGender(name) {
+  let apiURL = "https://api.genderize.io/?name=" + name;
+
+  fetch(apiURL)
+    .then(res => res.json())
+    .then(response => {
+      document.querySelector("p").innerHTML =
+        "the AI predicted gender of this actor is " + response.gender;
     });
 }
