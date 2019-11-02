8fcec337989bf19ab2db8e8b3057e52c27169e96
yasinallahdev <yasin.allahdev@gmail.com>
2019-11-02 19:13:45 -0400

    Adding Style

--- js/script.js
+++ js/script.js
@@ -1,29 +1,11 @@
 const breedSelect = document.querySelector("#breedSelect");
 const getImageButton = document.querySelector("#getImageButton");
+const catImage = document.querySelector("#catImage");
 
 getImageButton.addEventListener('click', showCatPicture);
 
 function showCatPicture() {
     const catBreed = breedSelect.options[breedSelect.selectedIndex].value;
-
-    /*
-
-        Got Initial Breeds from this, will remove later
-    
-    fetch(`https://api.thecatapi.com/v1/breeds`, {
-        headers: {
-            "x-api-key": catApiKey
-        }   
-    })
-        .then(res => res.json())
-        .then(response => {
-            let finalString = "<select>\n";
-            response.forEach(el => {
-                finalString += `\t<option value="${el.id}">${el.name}</option>\n`
-            })
-            finalString += "</select>"
-            console.log(finalString);
-        })*/
 
     fetch(`https://api.thecatapi.com/v1/images/search?breed_id=${catBreed}`, {
         headers: {
@@ -33,11 +15,13 @@ function showCatPicture() {
         .then(res => res.json())
         .then(response => {
             console.table(response);
-            fetch(`https://api.giphy.com/v1/gifs/search?api_key=${gifApiKey}&q=${response[0].breeds[0].name}&limit=1&offset=0&rating=G&lang=en`)
+            const catSearch = encodeURIComponent(`${response[0].breeds[0].name} cat`);
+            fetch(`https://api.giphy.com/v1/gifs/search?api_key=${gifApiKey}&q=${catSearch}&limit=1&offset=0&rating=G&lang=en`)
                 .then(res => res.json())
                 .then(response => {
                     console.table(response);
-                    document.querySelector("#catImage").src = response.data[0].images.downsized_large.url;
+                    catImage.src = response.data[0].images.downsized_large.url;
+                    catImage.alt = `${catBreed} gif`;
                 })
                 .catch(err => console.log(err));
         })
