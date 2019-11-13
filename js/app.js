cf7a48070230e940ec3a7e8bcc267c271fbf4df1
Kaniah Dunn <kaniahdunn@gmail.com>
2019-11-12 22:54:09 -0500

    updated second api call

--- js/app.js
+++ js/app.js
@@ -1,42 +1,36 @@
-//
-// let foodApiKey = "604ba12755ba28d8b52b8863c3cc4358"
-// fetch(`https://www.food2fork.com/api/search?key=${foodApiKey}&q=chicken%20breast&page=2`)
-//   .then(res => res.json())
-//   .then(response =>{
-//     console.log(response)
-//   })
-//   .catch(err=>{
-//     console.log(`error ${err}`)
-//   })
-const select = document.querySelector('.mealSelection')
-
-select.addEventListener('change', (event) => {
-
-fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
-  .then(res => res.json())
-  .then(response => {
-    let listCategory = response.categories
-    for (var i = 0; i < listCategory.length; i++) {
-    let mealCategory = listCategory[i].strCategory;
-    const select = document.querySelector('.mealSelection')
-    let option = document.createElement('option');
-    option.classList = mealCategory
-    option.appendChild(document.createTextNode( mealCategory));
-    select.appendChild(option);
-
-
-  let foodApiKey = "604ba12755ba28d8b52b8863c3cc4358"
-  fetch(`https://www.food2fork.com/api/search?key=${foodApiKey}&q=${select}&page=2`)
+const btn = document.querySelector('.button').addEventListener('click', ()=>{
+  const userInput = document.querySelector('.userInput').value
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${userInput}`)
     .then(res => res.json())
-    .then(response =>{
-      console.log(response)
+    .then(response => {
+      let ingredient = response.meals[0].strIngredient2;
+      let userIngredient = document.querySelector('.userIngredient')
+      userIngredient.innerHTML= ingredient
+
+      let foodApiKey = "604ba12755ba28d8b52b8863c3cc4358"
+        fetch(`https://www.food2fork.com/api/search?key=${foodApiKey}&q=${ingredient}&page=2`)
+          .then(res => res.json())
+          .then(response =>{
+            for (var i = 0; i < response.recipes.length; i++) {
+              let recipeUrl = response.recipes[i].source_url
+              let recipeTitles = response.recipes[i].title;
+
+              const list = document.querySelector('#recipe-list')
+
+              const row = document.createElement('tr')
+
+              row.innerHTML = `
+                <td>${recipeTitles}</td>
+                <td><a href="#" class='delete'>${recipeUrl}</a></td>
+              `
+              list.appendChild(row)
+            }
+          })
+          .catch(err=>{
+            console.log(`err ${err}`)
     })
     .catch(err=>{
-      console.log(`err ${err}`)
-  })
-  .catch(err=>{
-    console.log(`error ${err}`)
-  })
-}
+      console.log(`error ${err}`)
+    })
 })
 })
