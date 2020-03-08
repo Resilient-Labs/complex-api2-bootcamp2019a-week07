8d6c72d0b0e2069b09b1418d7c6d0f71787c3285
Vanessa Charles <vanessacharles@Vanessas-MacBook-Air.local>
2020-03-08 00:09:16 -0500

    complex api 2/nickmane

---
+++ js/main2.js
@@ -0,0 +1,10 @@
+// alert("fearless overlord")
+fetch(`https://www.behindthename.com/api/related.json?key=er246792804&${name}`)
+    .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
+    .then(response => {
+        console.log(response)
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+        alert("sorry, there are no results for your search")
+    });
